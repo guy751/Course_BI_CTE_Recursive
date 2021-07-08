8d2b0f3c2e510df047841711afa948adba6c4922
guy751 <84639497+guy751@users.noreply.github.com>
2021-07-08 21:54:07 +0300

    Add files via upload
    עדכון לתוכנית חישוב ספרת ביקורת של ת.ז.

--- חישוב ספרת ביקורת של תעודת זהות.sql
+++ חישוב ספרת ביקורת של תעודת זהות.sql
@@ -1,28 +1,32 @@
 --Cal Tz
--- check on two IDS : 20377121-7
+-- check on two IDS :   31511047-8
 -- the algorithm is described in Vikipedia under the Luhn algorithm
 
-
-declare @idStr  varchar (10)=  '20377121'-- the variable is written without the required digit since we want the CTE to calc the digit'
-select @idStr
+select 'Full ID Number is 31511047-8'
+declare @idStr  varchar (10)=  '31511047'-- the variable is written without the required digit since we want the CTE to calc the digit'
 
 ;with cte (num,pos,mul,sumOfDigits) 
 	as  (
 		 select 1 num,
 			substring(@idStr , 1 , 1 ) ,
-			cast(substring(@idStr , 1 , 1 ) as int) * ((1%2)+1) , 
-			cast(substring(@idStr , 1 , 1 ) as int) * ((1%2)+1) 
+			cast(substring(@idStr , 1 , 1 ) as int) * 1 , 
+			cast(substring(@idStr , 1 , 1 ) as int) * 1 
 
 		 union all 
 		 select num +1,  -- iteration number
 				substring(@idStr , num+1 , 1 ) pos , -- the digit in the Nth place
-				cast(substring(@idStr , num+1 , 1 ) as int)* (((num+1)%2)+1) mul , -- the digit in the Nth place multiplied by 1 or 2
-				sumOfDigits + cast(substring(@idStr , num+1 , 1 ) as int)* (((num+1)%2)+1) -- the sum of all the multiplations 
+				cast(substring(@idStr , num+1 , 1 ) as int)* ((num%2)+1) mul , -- the digit in the Nth place multiplied by 1 or 2
+				case when cast(substring(@idStr , num+1 , 1 ) as int)* ((num%2)+1) < 10
+				then
+					sumOfDigits + cast(substring(@idStr , num+1 , 1 ) as int)* ((num%2)+1) -- the sum of all the multiplations 
+				else
+					sumOfDigits + cast(substring(@idStr , num+1 , 1 ) as int)* ((num%2)+1) - 9  -- the sum of all the multiplations 
+				end
 		 from cte  
 				where num +1 < 9 -- assumption that the format of the string is valid
 		 )
-	--select * from cte  
-	select top 1 sumOfDigits % 10  from cte  order by sumOfDigits desc
+--	select * from cte  
+	select top 1 10 - (sumOfDigits % 10)  from cte  order by sumOfDigits desc
 
 
 
